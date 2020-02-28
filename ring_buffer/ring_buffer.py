c716db0c870ac8b7e961398bd450794dec1021b1
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2020-02-28 10:07:44 -0700

    added psuedocode for append and get for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,8 +7,31 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+# Append
+    # if the storage length is less than the max capacity
+        # add item to tail
+    # otherwise 
+        # if no node
+            # add item to the head 
+            # set current to head
+        # Otherwise
+            # if next of current
+                # insert item after head / current 
+                # set current as next
+                # delete the 'next' / item it is replacing
+            # otherwise if at end of list / back to begining of ring
+                # remove item from the head
+                # add the current item to the head
+                # set the current 'index' to head
     def append(self, item):
         pass 
+
+# GET
+    # initalize a tracker
+    # while there is a begining 
+        # assign list buffer content the values of of the tracker
+    # return the list buffer contents
+
 
     def get(self):
         # Note:  This is the only [] allowed
